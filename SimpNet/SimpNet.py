dd44177934c2fcac29a9bb30c5a59e45a345c987
Jordan Inturrisi <jordan.inturrisi@gmail.com>
2020-04-05 16:03:10 +1000

    run black

--- SimpNet/SimpNet.py
+++ SimpNet/SimpNet.py
@@ -1,34 +1,55 @@
 import keras
 
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, GlobalMaxPooling2D
-from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
-
+from keras.layers import (
+    Input,
+    Conv2D,
+    MaxPooling2D,
+    GlobalMaxPooling2D,
+    Dense,
+    Activation,
+    Flatten,
+    Dropout,
+    BatchNormalization,
+)
 from keras.initializers import glorot_normal, RandomNormal, Zeros
 
-# Define block which is a homogenous grouping of layers used to build SimpNet
+
 def block(name, x, F, K, act, init, pool=False):
-    out = Conv2D(filters=F, kernel_size=K, strides=1, use_bias=True,
-	padding='same', kernel_initializer=init, name=name)(x)
-    out = BatchNormalization(momentum=0.95, center=True, scale=True,
-	name=name+'_batchnorm')(out)
-    out = Activation(act, name=name+'_act')(out)
+    """
+    Define block which is a homogenous grouping of layers used to build SimpNet.
+    """
+    out = Conv2D(
+        filters=F,
+        kernel_size=K,
+        strides=1,
+        use_bias=True,
+        padding="same",
+        kernel_initializer=init,
+        name=name,
+    )(x)
+    out = BatchNormalization(
+        momentum=0.95, center=True, scale=True, name=name + "_batchnorm"
+    )(out)
+    out = Activation(act, name=name + "_act")(out)
 
-    if (pool == 'Max'):
-        out = MaxPooling2D(pool_size=2, strides=2, name=name+'_maxpool')(out)
-    elif (pool == 'Global'):
-        out = GlobalMaxPooling2D(name=name+'_globalpool')(out)
+    if pool == "Max":
+        out = MaxPooling2D(pool_size=2, strides=2, name=name + "_maxpool")(out)
+    elif pool == "Global":
+        out = GlobalMaxPooling2D(name=name + "_globalpool")(out)
 
-    out = Dropout(0.2, name=name+'_dropout')(out)
+    out = Dropout(0.2, name=name + "_dropout")(out)
 
     return out
 
-# SimpNet model for CIFAR10/100 with 8.9M parameters
+
 def SimpNet_CIFAR10_9M(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     """
     Keras implementation of SimpNet (arXiv: 1802.06205)
     (https://arxiv.org/abs/1802.06205)
 
+    SimpNet model for CIFAR10/100 with 8.9M parameters
+
     Inputs:
     - H: number of vertical pixels
     - W: number of horizontal pixels
@@ -39,62 +60,81 @@ def SimpNet_CIFAR10_9M(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     - model: SimpNet model
     """
 
-    input_img = Input(shape=[H,W,C], name='Input')
+    input_img = Input(shape=[H, W, C], name="Input")
 
     # Block 1
-    block1 = Conv2D(filters=128, kernel_size=3, strides=1, use_bias=True, padding='same', kernel_initializer=glorot_normal(), input_shape=[H,W,C], name='block1')(input_img)
-    block1 = BatchNormalization(momentum=0.95, center=True, scale=True, name='block1_batchnorm')(block1)
-    block1 = Activation(act, name='block1_act')(block1)
-    block1 = Dropout(0.2, name='block1_dropout')(block1)
+    block1 = Conv2D(
+        filters=128,
+        kernel_size=3,
+        strides=1,
+        use_bias=True,
+        padding="same",
+        kernel_initializer=glorot_normal(),
+        input_shape=[H, W, C],
+        name="block1",
+    )(input_img)
+    block1 = BatchNormalization(
+        momentum=0.95, center=True, scale=True, name="block1_batchnorm"
+    )(block1)
+    block1 = Activation(act, name="block1_act")(block1)
+    block1 = Dropout(0.2, name="block1_dropout")(block1)
 
     # Block 2
-    block2 = block('block2', block1, 182, 3, act, glorot_normal())
+    block2 = block("block2", block1, 182, 3, act, glorot_normal())
 
     # Block 3
-    block3 = block('block3', block2, 182, 3, act, RandomNormal(stddev=0.01))
+    block3 = block("block3", block2, 182, 3, act, RandomNormal(stddev=0.01))
 
     # Block 4
-    block4 = block('block4', block3, 182, 3, act, RandomNormal(stddev=0.01))
+    block4 = block("block4", block3, 182, 3, act, RandomNormal(stddev=0.01))
 
     # Block 5
-    block5 = block('block5', block4, 182, 3, act, RandomNormal(stddev=0.01), pool='Max')
+    block5 = block("block5", block4, 182, 3, act, RandomNormal(stddev=0.01), pool="Max")
 
     # Block 6
-    block6 = block('block6', block5, 182, 3, act, glorot_normal())
+    block6 = block("block6", block5, 182, 3, act, glorot_normal())
 
     # Block 7
-    block7 = block('block7', block6, 182, 3, act, glorot_normal())
+    block7 = block("block7", block6, 182, 3, act, glorot_normal())
 
     # Block 8
-    block8 = block('block8', block7, 182, 3, act, glorot_normal())
+    block8 = block("block8", block7, 182, 3, act, glorot_normal())
 
     # Block 9
-    block9 = block('block9', block8, 182, 3, act, glorot_normal())
+    block9 = block("block9", block8, 182, 3, act, glorot_normal())
 
     # Block 10
-    block10 = block('block10', block9, 430, 3, act, glorot_normal(), pool='Max')
+    block10 = block("block10", block9, 430, 3, act, glorot_normal(), pool="Max")
 
     # Block 11
-    block11 = block('block11', block10, 430, 3, act, glorot_normal())
+    block11 = block("block11", block10, 430, 3, act, glorot_normal())
 
     # Block 12
-    block12 = block('block12', block11, 455, 3, act, glorot_normal())
+    block12 = block("block12", block11, 455, 3, act, glorot_normal())
 
     # Block 13
-    block13 = block('block13', block12, 600, 3, act, glorot_normal(), pool='Global')
+    block13 = block("block13", block12, 600, 3, act, glorot_normal(), pool="Global")
 
     # Final Classifier
-    softmax = Dense(num_classes, activation='softmax', name='softmax', kernel_initializer=glorot_normal(), bias_initializer=Zeros())(block13)
+    softmax = Dense(
+        num_classes,
+        activation="softmax",
+        name="softmax",
+        kernel_initializer=glorot_normal(),
+        bias_initializer=Zeros(),
+    )(block13)
 
     return Model(inputs=input_img, outputs=softmax)
 
-# SimpNet model for CIFAR10/100 with 5M parameters
-# Also SimpNet model for SVHN (Street View House Numbers)
+
 def SimpNet_CIFAR10_5M(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     """
     Keras implementation of SimpNet (arXiv: 1802.06205)
     (https://arxiv.org/abs/1802.06205)
 
+    SimpNet model for CIFAR10/100 with 5M parameters
+    Also SimpNet model for SVHN (Street View House Numbers)
+
     Inputs:
     - H: number of vertical pixels
     - W: number of horizontal pixels
@@ -105,61 +145,80 @@ def SimpNet_CIFAR10_5M(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     - model: SimpNet model
     """
 
-    input_img = Input(shape=[H,W,C], name='Input')
+    input_img = Input(shape=[H, W, C], name="Input")
 
     # Block 1
-    block1 = Conv2D(filters=66, kernel_size=3, strides=1, use_bias=True, padding='same', kernel_initializer=glorot_normal(), input_shape=[H,W,C], name='block1')(input_img)
-    block1 = BatchNormalization(momentum=0.95, center=True, scale=True, name='block1_batchnorm')(block1)
-    block1 = Activation(act, name='block1_act')(block1)
-    block1 = Dropout(0.2, name='block1_dropout')(block1)
+    block1 = Conv2D(
+        filters=66,
+        kernel_size=3,
+        strides=1,
+        use_bias=True,
+        padding="same",
+        kernel_initializer=glorot_normal(),
+        input_shape=[H, W, C],
+        name="block1",
+    )(input_img)
+    block1 = BatchNormalization(
+        momentum=0.95, center=True, scale=True, name="block1_batchnorm"
+    )(block1)
+    block1 = Activation(act, name="block1_act")(block1)
+    block1 = Dropout(0.2, name="block1_dropout")(block1)
 
     # Block 2
-    block2 = block('block2', block1, 128, 3, act, glorot_normal())
+    block2 = block("block2", block1, 128, 3, act, glorot_normal())
 
     # Block 3
-    block3 = block('block3', block2, 128, 3, act, RandomNormal(stddev=0.01))
+    block3 = block("block3", block2, 128, 3, act, RandomNormal(stddev=0.01))
 
     # Block 4
-    block4 = block('block4', block3, 128, 3, act, RandomNormal(stddev=0.01))
+    block4 = block("block4", block3, 128, 3, act, RandomNormal(stddev=0.01))
 
     # Block 5
-    block5 = block('block5', block4, 192, 3, act, RandomNormal(stddev=0.01), pool='Max')
+    block5 = block("block5", block4, 192, 3, act, RandomNormal(stddev=0.01), pool="Max")
 
     # Block 6
-    block6 = block('block6', block5, 192, 3, act, glorot_normal())
+    block6 = block("block6", block5, 192, 3, act, glorot_normal())
 
     # Block 7
-    block7 = block('block7', block6, 192, 3, act, glorot_normal())
+    block7 = block("block7", block6, 192, 3, act, glorot_normal())
 
     # Block 8
-    block8 = block('block8', block7, 192, 3, act, glorot_normal())
+    block8 = block("block8", block7, 192, 3, act, glorot_normal())
 
     # Block 9
-    block9 = block('block9', block8, 192, 3, act, glorot_normal())
+    block9 = block("block9", block8, 192, 3, act, glorot_normal())
 
     # Block 10
-    block10 = block('block10', block9, 288, 3, act, glorot_normal(), pool='Max')
+    block10 = block("block10", block9, 288, 3, act, glorot_normal(), pool="Max")
 
     # Block 11
-    block11 = block('block11', block10, 288, 3, act, glorot_normal())
+    block11 = block("block11", block10, 288, 3, act, glorot_normal())
 
     # Block 12
-    block12 = block('block12', block11, 355, 3, act, glorot_normal())
+    block12 = block("block12", block11, 355, 3, act, glorot_normal())
 
     # Block 13
-    block13 = block('block13', block12, 432, 3, act, glorot_normal(), pool='Global')
+    block13 = block("block13", block12, 432, 3, act, glorot_normal(), pool="Global")
 
     # Final Classifier
-    softmax = Dense(num_classes, activation='softmax', name='softmax', kernel_initializer=glorot_normal(), bias_initializer=Zeros())(block13)
+    softmax = Dense(
+        num_classes,
+        activation="softmax",
+        name="softmax",
+        kernel_initializer=glorot_normal(),
+        bias_initializer=Zeros(),
+    )(block13)
 
     return Model(inputs=input_img, outputs=softmax)
 
-# SimpNet model for MNIST
+
 def SimpNet_MNIST(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     """
     Keras implementation of SimpNet (arXiv: 1802.06205)
     (https://arxiv.org/abs/1802.06205)
 
+    SimpNet model for MNIST.
+
     Inputs:
     - H: number of vertical pixels
     - W: number of horizontal pixels
@@ -170,51 +229,68 @@ def SimpNet_MNIST(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     - model: SimpNet model
     """
 
-    input_img = Input(shape=[H,W,C], name='Input')
+    input_img = Input(shape=[H, W, C], name="Input")
 
     # Block 1
-    block1 = Conv2D(filters=66, kernel_size=3, strides=1, use_bias=True, padding='same', kernel_initializer=glorot_normal(), input_shape=[H,W,C], name='block1')(input_img)
-    block1 = BatchNormalization(momentum=0.95, center=True, scale=True, name='block1_batchnorm')(block1)
-    block1 = Activation(act, name='block1_act')(block1)
-    block1 = Dropout(0.2, name='block1_dropout')(block1)
+    block1 = Conv2D(
+        filters=66,
+        kernel_size=3,
+        strides=1,
+        use_bias=True,
+        padding="same",
+        kernel_initializer=glorot_normal(),
+        input_shape=[H, W, C],
+        name="block1",
+    )(input_img)
+    block1 = BatchNormalization(
+        momentum=0.95, center=True, scale=True, name="block1_batchnorm"
+    )(block1)
+    block1 = Activation(act, name="block1_act")(block1)
+    block1 = Dropout(0.2, name="block1_dropout")(block1)
 
     # Block 2
-    block2 = block('block2', block1, 64, 3, act, glorot_normal())
+    block2 = block("block2", block1, 64, 3, act, glorot_normal())
 
     # Block 3
-    block3 = block('block3', block2, 64, 3, act, RandomNormal(stddev=0.01))
+    block3 = block("block3", block2, 64, 3, act, RandomNormal(stddev=0.01))
 
     # Block 4
-    block4 = block('block4', block3, 64, 3, act, RandomNormal(stddev=0.01))
+    block4 = block("block4", block3, 64, 3, act, RandomNormal(stddev=0.01))
 
     # Block 5
-    block5 = block('block5', block4, 96, 3, act, RandomNormal(stddev=0.01), pool='Max')
+    block5 = block("block5", block4, 96, 3, act, RandomNormal(stddev=0.01), pool="Max")
 
     # Block 6
-    block6 = block('block6', block5, 96, 3, act, glorot_normal())
+    block6 = block("block6", block5, 96, 3, act, glorot_normal())
 
     # Block 7
-    block7 = block('block7', block6, 96, 3, act, glorot_normal())
+    block7 = block("block7", block6, 96, 3, act, glorot_normal())
 
     # Block 8
-    block8 = block('block8', block7, 96, 3, act, glorot_normal())
+    block8 = block("block8", block7, 96, 3, act, glorot_normal())
 
     # Block 9
-    block9 = block('block9', block8, 96, 3, act, glorot_normal())
+    block9 = block("block9", block8, 96, 3, act, glorot_normal())
 
     # Block 10
-    block10 = block('block10', block9, 144, 3, act, glorot_normal(), pool='Max')
+    block10 = block("block10", block9, 144, 3, act, glorot_normal(), pool="Max")
 
     # Block 11
-    block11 = block('block11', block10, 144, 3, act, glorot_normal())
+    block11 = block("block11", block10, 144, 3, act, glorot_normal())
 
     # Block 12
-    block12 = block('block12', block11, 178, 3, act, glorot_normal())
+    block12 = block("block12", block11, 178, 3, act, glorot_normal())
 
     # Block 13
-    block13 = block('block13', block12, 216, 3, act, glorot_normal(), pool='Global')
+    block13 = block("block13", block12, 216, 3, act, glorot_normal(), pool="Global")
 
     # Final Classifier
-    softmax = Dense(num_classes, activation='softmax', name='softmax', kernel_initializer=glorot_normal(), bias_initializer=Zeros())(block13)
+    softmax = Dense(
+        num_classes,
+        activation="softmax",
+        name="softmax",
+        kernel_initializer=glorot_normal(),
+        bias_initializer=Zeros(),
+    )(block13)
 
     return Model(inputs=input_img, outputs=softmax)
--- SqueezeNet/SqueezeNet.py
+++ SqueezeNet/SqueezeNet.py
@@ -1,7 +1,14 @@
 from keras.models import Model
-from keras.layers import Input, Activation, Concatenate, Conv2D
-from keras.layers import MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout
+from keras.layers import (
+    Input,
+    Activation,
+    Concatenate,
+    Conv2D,
+    MaxPooling2D,
+    GlobalAveragePooling2D,
+    Dropout,
+)
+
 
 def fire_module(name, x, sp, e11p, e33p, act, data_format, init):
     """
@@ -20,21 +27,42 @@ def fire_module(name, x, sp, e11p, e33p, act, data_format, init):
     Returns:
     - output: output of fire module
     """
-    squeeze = Conv2D(filters=sp, kernel_size=[1,1], activation=act,
-    kernel_initializer=init, padding='same', name=name+'_squeeze',
-    use_bias='True', data_format=data_format)(x)
+    squeeze = Conv2D(
+        filters=sp,
+        kernel_size=[1, 1],
+        activation=act,
+        kernel_initializer=init,
+        padding="same",
+        name=name + "_squeeze",
+        use_bias="True",
+        data_format=data_format,
+    )(x)
 
-    e11 = Conv2D(filters=e11p, kernel_size=[1,1], activation=act,
-    kernel_initializer=init, padding='same', name=name+'_e11',
-    use_bias='True', data_format=data_format)(squeeze)
+    e11 = Conv2D(
+        filters=e11p,
+        kernel_size=[1, 1],
+        activation=act,
+        kernel_initializer=init,
+        padding="same",
+        name=name + "_e11",
+        use_bias="True",
+        data_format=data_format,
+    )(squeeze)
 
-    e33 = Conv2D(filters=e33p, kernel_size=[3,3], activation=act,
-    kernel_initializer=init, padding='same', name=name+'_e33',
-    use_bias='True', data_format=data_format)(squeeze)
+    e33 = Conv2D(
+        filters=e33p,
+        kernel_size=[3, 3],
+        activation=act,
+        kernel_initializer=init,
+        padding="same",
+        name=name + "_e33",
+        use_bias="True",
+        data_format=data_format,
+    )(squeeze)
 
-    if (data_format == 'channels_first'):
+    if data_format == "channels_first":
         output = Concatenate(axis=1)([e11, e33])
-    elif (data_format == 'channels_last'):
+    elif data_format == "channels_last":
         output = Concatenate(axis=3)([e11, e33])
 
     return output
@@ -57,53 +85,70 @@ def SqueezeNet_v1_0(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     - model: SqueezeNet v1.0 model
     """
 
-    data_format = 'channels_last'
-    init = 'glorot_uniform'
+    data_format = "channels_last"
+    init = "glorot_uniform"
 
-    if (data_format == 'channels_first'):
-        input_img = Input(shape=[C,H,W], name='Input')
-    elif (data_format == 'channels_last'):
-        input_img = Input(shape=[H,W,C], name='Input')
+    if data_format == "channels_first":
+        input_img = Input(shape=[C, H, W], name="Input")
+    elif data_format == "channels_last":
+        input_img = Input(shape=[H, W, C], name="Input")
     else:
-        print('ValueError: The `data_format` argument must be one of "channels_first", "channels_last"')
+        print(
+            'ValueError: The `data_format` argument must be one of "channels_first", "channels_last"'
+        )
 
-    conv1 = Conv2D(filters=96, kernel_size=[7,7], activation=act,
-    kernel_initializer=init, strides=[2,2], padding='same',
-    name='conv1', data_format=data_format)(input_img)
+    conv1 = Conv2D(
+        filters=96,
+        kernel_size=[7, 7],
+        activation=act,
+        kernel_initializer=init,
+        strides=[2, 2],
+        padding="same",
+        name="conv1",
+        data_format=data_format,
+    )(input_img)
 
-    maxpool1 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool1',
-    data_format=data_format)(conv1)
+    maxpool1 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool1", data_format=data_format
+    )(conv1)
 
-    fire2 = fire_module('fire2', maxpool1, 16, 64, 64, act, data_format, init)
+    fire2 = fire_module("fire2", maxpool1, 16, 64, 64, act, data_format, init)
 
-    fire3 = fire_module('fire3', fire2, 16, 64, 64, act, data_format, init)
+    fire3 = fire_module("fire3", fire2, 16, 64, 64, act, data_format, init)
 
-    fire4 = fire_module('fire4', fire3, 32, 128, 128, act, data_format, init)
+    fire4 = fire_module("fire4", fire3, 32, 128, 128, act, data_format, init)
 
-    maxpool4 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool4',
-    data_format=data_format)(fire4)
+    maxpool4 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool4", data_format=data_format
+    )(fire4)
 
-    fire5 = fire_module('fire5', maxpool4, 32, 128, 128, act, data_format, init)
+    fire5 = fire_module("fire5", maxpool4, 32, 128, 128, act, data_format, init)
 
-    fire6 = fire_module('fire6', fire5, 48, 192, 192, act, data_format, init)
+    fire6 = fire_module("fire6", fire5, 48, 192, 192, act, data_format, init)
 
-    fire7 = fire_module('fire7', fire6, 48, 192, 192, act, data_format, init)
+    fire7 = fire_module("fire7", fire6, 48, 192, 192, act, data_format, init)
 
-    fire8 = fire_module('fire8', fire7, 64, 256, 256, act, data_format, init)
+    fire8 = fire_module("fire8", fire7, 64, 256, 256, act, data_format, init)
 
-    maxpool8 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool8',
-    data_format=data_format)(fire8)
+    maxpool8 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool8", data_format=data_format
+    )(fire8)
 
-    fire9 = fire_module('fire9', maxpool8, 64, 256, 256, act, data_format, init)
+    fire9 = fire_module("fire9", maxpool8, 64, 256, 256, act, data_format, init)
 
-    fire9_dropout = Dropout(rate=0.5, name='fire9_dropout')(fire9)
+    fire9_dropout = Dropout(rate=0.5, name="fire9_dropout")(fire9)
 
-    conv10 = Conv2D(filters=num_classes, kernel_size=[1,1],
-    kernel_initializer=init, padding='valid', name='conv10',
-    data_format=data_format)(fire9_dropout)
+    conv10 = Conv2D(
+        filters=num_classes,
+        kernel_size=[1, 1],
+        kernel_initializer=init,
+        padding="valid",
+        name="conv10",
+        data_format=data_format,
+    )(fire9_dropout)
 
     global_avgpool10 = GlobalAveragePooling2D(data_format=data_format)(conv10)
-    softmax = Activation(activation="softmax", name='softmax')(global_avgpool10)
+    softmax = Activation(activation="softmax", name="softmax")(global_avgpool10)
 
     return Model(inputs=input_img, outputs=softmax)
 
@@ -125,52 +170,69 @@ def SqueezeNet_v1_1(H, W, C, num_classes, act, drop_rate=0.25, reg=0):
     - model: SqueezeNet v1.1 model
     """
 
-    data_format = 'channels_last'
-    init = 'glorot_uniform'
+    data_format = "channels_last"
+    init = "glorot_uniform"
 
-    if (data_format == 'channels_first'):
-        input_img = Input(shape=[C,H,W], name='Input')
-    elif (data_format == 'channels_last'):
-        input_img = Input(shape=[H,W,C], name='Input')
+    if data_format == "channels_first":
+        input_img = Input(shape=[C, H, W], name="Input")
+    elif data_format == "channels_last":
+        input_img = Input(shape=[H, W, C], name="Input")
     else:
-        print('ValueError: The `data_format` argument must be one of "channels_first", "channels_last"')
+        print(
+            'ValueError: The `data_format` argument must be one of "channels_first", "channels_last"'
+        )
 
-    conv1 = Conv2D(filters=64, kernel_size=[3,3], activation=act,
-    kernel_initializer=init, strides=[2,2], padding='same',
-    name='conv1', data_format=data_format)(input_img)
+    conv1 = Conv2D(
+        filters=64,
+        kernel_size=[3, 3],
+        activation=act,
+        kernel_initializer=init,
+        strides=[2, 2],
+        padding="same",
+        name="conv1",
+        data_format=data_format,
+    )(input_img)
 
-    maxpool1 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool1',
-    data_format=data_format)(conv1)
+    maxpool1 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool1", data_format=data_format
+    )(conv1)
 
-    fire2 = fire_module('fire2', maxpool1, 16, 64, 64, act, data_format, init)
+    fire2 = fire_module("fire2", maxpool1, 16, 64, 64, act, data_format, init)
 
-    fire3 = fire_module('fire3', fire2, 16, 64, 64, act, data_format, init)
+    fire3 = fire_module("fire3", fire2, 16, 64, 64, act, data_format, init)
 
-    maxpool3 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool3',
-    data_format=data_format)(fire3)
+    maxpool3 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool3", data_format=data_format
+    )(fire3)
 
-    fire4 = fire_module('fire4', maxpool3, 32, 128, 128, act, data_format, init)
+    fire4 = fire_module("fire4", maxpool3, 32, 128, 128, act, data_format, init)
 
-    fire5 = fire_module('fire5', fire4, 32, 128, 128, act, data_format, init)
+    fire5 = fire_module("fire5", fire4, 32, 128, 128, act, data_format, init)
 
-    maxpool5 = MaxPooling2D(pool_size=[3,3], strides=[2,2], name='maxpool5',
-    data_format=data_format)(fire5)
+    maxpool5 = MaxPooling2D(
+        pool_size=[3, 3], strides=[2, 2], name="maxpool5", data_format=data_format
+    )(fire5)
 
-    fire6 = fire_module('fire6', maxpool5, 48, 192, 192, act, data_format, init)
+    fire6 = fire_module("fire6", maxpool5, 48, 192, 192, act, data_format, init)
 
-    fire7 = fire_module('fire7', fire6, 48, 192, 192, act, data_format, init)
+    fire7 = fire_module("fire7", fire6, 48, 192, 192, act, data_format, init)
 
-    fire8 = fire_module('fire8', fire7, 64, 256, 256, act, data_format, init)
+    fire8 = fire_module("fire8", fire7, 64, 256, 256, act, data_format, init)
 
-    fire9 = fire_module('fire9', fire8, 64, 256, 256, act, data_format, init)
+    fire9 = fire_module("fire9", fire8, 64, 256, 256, act, data_format, init)
 
-    fire9_dropout = Dropout(rate=0.5, name='fire9_dropout')(fire9)
+    fire9_dropout = Dropout(rate=0.5, name="fire9_dropout")(fire9)
 
-    conv10 = Conv2D(filters=num_classes, kernel_size=[1,1],
-    kernel_initializer=init, padding='valid', name='conv10',
-    data_format=data_format)(fire9_dropout)
+    conv10 = Conv2D(
+        filters=num_classes,
+        kernel_size=[1, 1],
+        kernel_initializer=init,
+        padding="valid",
+        name="conv10",
+        data_format=data_format,
+    )(fire9_dropout)
 
     global_avgpool10 = GlobalAveragePooling2D(data_format=data_format)(conv10)
-    softmax = Activation(activation="softmax", name='softmax')(global_avgpool10)
+    softmax = Activation(activation="softmax", name="softmax")(global_avgpool10)
 
     return Model(inputs=input_img, outputs=softmax)
